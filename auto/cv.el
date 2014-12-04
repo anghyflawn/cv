982fa11e369b937dc7c813be486db7ce99b2f153
Pavel Iosad <pavel.iosad@ed.ac.uk>
2014-12-04 20:54:40 +0000

    RSE grant

--- auto/cv.el
+++ auto/cv.el
@@ -1,35 +1,34 @@
-(TeX-add-style-hook "cv"
+(TeX-add-style-hook
+ "cv"
  (lambda ()
-    (LaTeX-add-labels
-     "sec:edited-volumes")
-    (TeX-add-symbols
-     '("yearblock" ["argument"] 1)
-     '("block" 2)
-     '("cvitem" 1))
-    (TeX-run-style-hooks
-     "hyperref"
-     "etoolbox"
-     "multicol"
-     "calc"
-     "titlesec"
-     "xcolor"
-     "dvipsnames"
-     "usenames"
-     "csquotes"
-     "english=british"
-     "autostyle"
-     "polyglossia"
-     "etex"
-     "graphicx"
-     "color"
-     "parskip"
-     "xltxtra"
-     ""
-     "typearea"
-     "DIV13"
-     "latex2e"
-     "art11"
-     "article"
-     "11pt"
-     "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("typearea" "DIV13") ("csquotes" "autostyle" "english=british") ("xcolor" "usenames" "dvipsnames")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "typearea"
+    "xltxtra"
+    "parskip"
+    "color"
+    "graphicx"
+    "etex"
+    "polyglossia"
+    "csquotes"
+    "xcolor"
+    "titlesec"
+    "calc"
+    "multicol"
+    "etoolbox"
+    "hyperref")
+   (TeX-add-symbols
+    '("yearblock" ["argument"] 1)
+    '("block" 2)
+    '("cvitem" 1))
+   (LaTeX-add-labels
+    "sec:edited-volumes")
+   (LaTeX-add-lengths
+    "blockwidth")))
 
