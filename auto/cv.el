480c28a674c0e230135eff0c93ad9da4339c5b8d
Pavel Iosad <pavel.iosad@ed.ac.uk>
2016-02-20 16:22:41 +0100

    Add done FiSk

--- auto/cv.el
+++ auto/cv.el
@@ -5,14 +5,13 @@
                      '(("article" "a4paper" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("typearea" "DIV13") ("csquotes" "autostyle" "english=british") ("xcolor" "usenames" "dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -42,5 +41,6 @@
    (LaTeX-add-lengths
     "blockwidth")
    (LaTeX-add-polyglossia-langs
-    '("english" "mainlanguage" "variant=british"))))
+    '("english" "mainlanguage" "variant=british")))
+ :latex)
 
