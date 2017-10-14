8ccc2549e30fa6244b5229cf785875234a373f80
Pavel Iosad <pavel.iosad@ed.ac.uk>
2017-10-14 14:50:00 +0100

    Talks etc.

--- auto/cv.el
+++ auto/cv.el
@@ -4,13 +4,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "a4paper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("typearea" "DIV12") ("csquotes" "autostyle" "english=british") ("xcolor" "usenames" "dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("typearea" "DIV11") ("babel" "main=british" "russian") ("csquotes" "autostyle") ("xcolor" "usenames" "dvipsnames")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -22,7 +22,7 @@
     "color"
     "graphicx"
     "etex"
-    "polyglossia"
+    "babel"
     "csquotes"
     "xcolor"
     "titlesec"
@@ -35,18 +35,14 @@
     '("doi" 1)
     '("block" 2)
     '("cvitem" 1)
-    '("textitrussian" 1))
+    '("textitrussian" 1)
+    '("textrussian" 1))
    (LaTeX-add-labels
     "sec:books"
     "sec:edited-volumes"
     "sec:misc-publ"
     "sec:phd-supervision")
    (LaTeX-add-lengths
-    "blockwidth")
-   (LaTeX-add-fontspec-newfontcmds
-    "russianfont")
-   (LaTeX-add-polyglossia-langs
-    '("english" "mainlanguage" "variant=british")
-    '("russian" "otherlanguage" "")))
+    "blockwidth"))
  :latex)
 
