6d6d8ba31488cf9c6af1a4a5928461475fa21d26
Pavel Iosad <pavel.iosad@ed.ac.uk>
2018-05-03 21:05:20 +0100

    Cleanup, extra pubs

--- auto/cv.el
+++ auto/cv.el
@@ -4,20 +4,19 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "a4paper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("typearea" "DIV=11") ("babel" "main=british" "russian") ("csquotes" "autostyle") ("xcolor" "usenames" "dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+                     '(("typearea" "DIV=11") ("babel" "main=british" "russian") ("csquotes" "autostyle") ("xcolor" "usenames" "dvipsnames") ("inputenc" "utf8") ("fontenc" "T2A" "T1") ("cantarell" "defaultsans")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art11"
     "typearea"
-    "luatextra"
     "parskip"
     "color"
     "graphicx"
@@ -29,7 +28,15 @@
     "calc"
     "multicol"
     "etoolbox"
-    "hyperref")
+    "hyperref"
+    "ifluatex"
+    "luatextra"
+    "inputenc"
+    "fontenc"
+    "tipa"
+    "literat"
+    "cantarell"
+    "newunicodechar")
    (TeX-add-symbols
     '("yearblock" ["argument"] 1)
     '("doi" 1)
@@ -37,6 +44,7 @@
     '("cvitem" 1)
     '("textitrussian" 1)
     '("textrussian" 1)
+    '("ipa" 1)
     "ipa")
    (LaTeX-add-labels
     "sec:books"
